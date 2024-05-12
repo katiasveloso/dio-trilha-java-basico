c5938086a677b475d04510d08345304b01303def
katia <katisv@gmail.com>
2024-05-12 16:00:16 -0300

    Estudo anatomia das classes

--- AnatomiadasClasses/src/classes05.java
+++ AnatomiadasClasses/src/classes05.java
@@ -12,3 +12,9 @@ public class classes05 {
             }
     
 }
+
+
+//Indentação (indentation)
+//Basicamente indentar é um termo utilizado para escrever o código do 
+//programa de forma hierárquica, facilitando assim a visualização e o entendimento 
+//do programa.
