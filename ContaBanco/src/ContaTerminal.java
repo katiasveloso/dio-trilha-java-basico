6c403010868c33591d96f75d38fb1b1449f1559e
katia <katisv@gmail.com>
2024-05-17 19:56:43 -0300

    Projeto Conta Bancaria

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+       int numero=1021;
+       String agencia= "067-8";
+       String NomeCliente= "Katia Silva Veloso";
+       float saldo = (float) 237.48;
+       
+
+
+
+    }
+}
